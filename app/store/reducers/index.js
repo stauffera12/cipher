929f647b61d65f08c3ba6e11dc4ecc188047431b
stauffera12 <austin.lee.stauffer@gmail.com>
2025-02-27 11:11:48 -0500

    Restructure project and remove old files

--- app/store/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-
-const rootReducer = combineReducers({
-  auth: authReducer,
-  // Add other reducers here as your app grows
-});
-
-export default rootReducer;
